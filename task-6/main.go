c8d0883f3429f8974f6023f133455156e2f4ca0d
AlexVorobushek <alex080605@mail.ru>
2023-11-08 13:09:40 +0300

    review task-6

--- task-6/main.go
+++ task-6/main.go
@@ -1,33 +1,29 @@
 package main
 
 import (
-	"fmt"
-	"strings"
 	"bufio"
-  	"os"
+	"fmt"
+	"os"
+	"strings"
 )
 
 func stringInSlice(a string, list []string) bool {
-    for _, b := range list {
-        if b == a {
-            return true
-        }
-    }
-    return false
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
 }
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
-    text, _ := reader.ReadString('\n')
+	text, _ := reader.ReadString('\n')
 	items := strings.Split(text, " ")
 
-	var already_have []string
-	var ans int
-	for _, str := range items{
-		if ! stringInSlice(str, already_have) {
-			ans++
-			already_have = append(already_have, str)
-		}
+	alreadyHave := make(map[string]bool)
+	for _, str := range items {
+		alreadyHave[str] = true
 	}
-	fmt.Println(ans)
+	fmt.Println(len(alreadyHave))
 }
